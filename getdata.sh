a910d2802899b8e63cabc866784f8c0ff9bf0dee
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-29 00:28:33 -0500

    Update getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -26,12 +26,12 @@ fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001847
 fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001846
 
 # Download and unpack the short read reference genome and gff file.
-URL1=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/151/805/GCA_000151805.2_Taeniopygia_guttata-3.2.4/GCA_000151805.2_Taeniopygia_guttata-3.2.4_genomic.fna.gz
+URL1=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/151/805/GCF_000151805.1_Taeniopygia_guttata-3.2.4/GCF_000151805.1_Taeniopygia_guttata-3.2.4_genomic.fna.gz
 echo "*** Downloading: $URL1"
 curl -s $URL1 > srr/SRR.fa.gz
 gunzip $REFSRR
 
-URL2=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/151/805/GCA_000151805.2_Taeniopygia_guttata-3.2.4/GCA_000151805.2_Taeniopygia_guttata-3.2.4_genomic.gff.gz
+URL2=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/151/805/GCF_000151805.1_Taeniopygia_guttata-3.2.4/GCF_000151805.1_Taeniopygia_guttata-3.2.4_genomic.gff.gz
 echo "*** Downloading: $URL2"
 curl -s $URL2 > srr/SRR.gff.gz
 gunzip $GFFSRR

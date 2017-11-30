0fde659aab991d5ab68309b62ad0d833c96c7397
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-29 23:20:16 -0500

    Update countdata.sh

--- countdata.sh
+++ countdata.sh
@@ -4,9 +4,9 @@ gffread srr/SRR.gff -T -o srr/SRR.gtf
 GTFSRR=srr/SRR.gtf
 
 echo "*** Counting features with: $GTFSRR"
-featureCounts -T 16 -a $GTFSRR -g gene_name -o counts.txt  bam1/C*.bam  bam1/T2_*.bam bam1/T4_*.bam
+featureCounts -T 16 -a $GTFSRR -g gene_name -o srrcounts.txt  bam1/C*.bam  bam1/T2_*.bam bam1/T4_*.bam
 
 #simple counts
-cat counts.txt | cut -f 1,7-15 > simple_counts.txt
+cat counts.txt | cut -f 1,7-15 > finalsrrcounts.txt
 
 ls

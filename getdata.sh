e17c094fd0ba9ed23ececaf5883d883eec0ff889
barbolute <33287182+barbolute@users.noreply.github.com>
2018-10-14 23:17:13 -0400

    Update getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -19,14 +19,14 @@ GFFSRR=srr/SRR.gff*
 # Download and unpack the raw RNA-Seq Data for the DE part of experiment
 #from spleen
 #control
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001851
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001848
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001850
+fastq-dump --gzip --split-files -O rawdata SRR5001851
+fastq-dump --gzip --split-files -O rawdata SRR5001848
+fastq-dump --gzip --split-files -O rawdata SRR5001850
 
 #2 DPI
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001849
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001845
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001843
+fastq-dump --gzip --split-files -O rawdata SRR5001849
+fastq-dump --gzip --split-files -O rawdata SRR5001845
+fastq-dump --gzip --split-files -O rawdata SRR5001843
 
 fastq-dump --gzip --split-files -O rawdata SRR5001849
 fastq-dump --gzip --split-files -O rawdata SRR5001845
@@ -34,9 +34,9 @@ fastq-dump --gzip --split-files -O rawdata SRR5001843
 ls
 
 #4 DPI
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001844
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001847
-fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001846
+fastq-dump --gzip --split-files -O rawdata SRR5001844
+fastq-dump --gzip --split-files -O rawdata SRR5001847
+fastq-dump --gzip --split-files -O rawdata SRR5001846
 
 # Download and unpack the short read reference genome and gff file.
 URL1=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/151/805/GCF_000151805.1_Taeniopygia_guttata-3.2.4/GCF_000151805.1_Taeniopygia_guttata-3.2.4_genomic.fna.gz
